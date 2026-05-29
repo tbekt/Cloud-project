78a208bc5f974f17b2fe75fbaa89ce0939986c7c
Taner Bektas <taner.bektas03@gmail.com>
2026-05-30 01:49:12 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Taner project werkt! </h1><p>De website is live via Docker en Caddy.</p>');
+    res.end('<h1>Taner project werkt!!!! </h1><p>De website is live via Docker en Caddy.</p>');
 });
 
 server.listen(3000, () => {
