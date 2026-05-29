97f90a113359b67aabdf84f751f438a9009da454
Taner Bektas <taner.bektas03@gmail.com>
2026-05-29 23:27:18 +0200

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Taner project werkt! 🎉</h1><p>De website is live via Docker en Caddy.</p>');
+});
+
+server.listen(3000, () => {
+    console.log('App draait op poort 3000');
+});
